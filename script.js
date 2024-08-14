3465d1727424a9305f9623f0021264eb2dc7b5ec
AkkySharma525 <theakshitsharma525@gmail.com>
2024-08-14 15:39:44 +0530

    "INITIAL COMMIT OF WEBSIT FILE"

---
+++ script.js
@@ -0,0 +1,11 @@
+// Add any interactive features you want here
+// For example, smooth scrolling between sections
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
